54734f4f82bd8db802c9da5ae2db08a0d5072ea5
hanmowang <hanmow2003@gmail.com>
2024-02-17 20:36:40 -0500

    add backend

---
+++ nba-backend/src/test/java/com/nbastats/nbabackend/NbaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbastats.nbabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
